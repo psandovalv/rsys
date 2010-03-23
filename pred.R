7848685c865e40851040518c41e8de129658b3bb
Suchindra Sandhu <suchindra@gmail.com>
2010-03-23 16:38:55 -0400

    Linear model example

--- pred.R
+++ pred.R
@@ -11,6 +11,7 @@ target[target >= 0] <- 1
 target[target < 0] <- -1
 
 Train <- function(features, target, ntrain=30, npred=1, debug=FALSE) {
+  ## Dumb linear model
   ## Assumes that data is ordered from oldest to newest
   ## features is the input timeseries(xts) to train on
   ## target is the target timeseries(xts)
@@ -19,30 +20,28 @@ Train <- function(features, target, ntrain=30, npred=1, debug=FALSE) {
 
   iend <- nrow(features) - ntrain - npred - 1
   df <- merge(features, lag(target, -npred))
-  preds = rep(0, iend)
+  colnames(df) <- append(colnames(features), "class")
+
+  preds <- rep(0, iend)
+  actual <- rep(0, iend)
 
   for (i in 1:iend) {
-    window <- i:(i + ntrain - 1)
-    train <- features[window, ]
-    class <- target[(window + npred), ]
+    j <- (i + ntrain - 1)
+    train <- df[i:j, ]
 
-    if (debug) {
+    if (debug)
       cat("train ", paste(start(train), end(train), sep = " to "), "\n")
-      cat("class ", paste(start(class), end(class), sep = " to "), "\n")
-    }
-    df <- merge(train, class)
-    rownames(df) <- append(rownames(train), "class")
-    mod <- lm(class ~ train$FCXr1 + train$FCXr4 + train$FCXHigh.Low. +
-              train$FCXVolNorm + train$DHYr1 + train$XLFr1 + train$SPYr1 +
-              train$GLDr1 + train$RTPr1)
-    cat("Index ", i, "\n")
-    preds[i] <- predict(mod, features[(length(window) + 1), ])
-    print(preds)
-  }
 
-  return(preds)
+    mod <- lm(class ~ ., data=train)
+    preds[i] <- predict(mod, df[(j + 1), ])
+    actual[i] <- df$class[j + 1]
+  }
+  return(data.frame("actual"=actual, "forecast"=preds))
 }
 
-Train(data, target)
+res <- Train(data, target)
+res$forecast[res$forecast > 0] <- 1
+res$forecast[res$forecast < 0] <- -1
 
+cat("Test Error is ", sum(res$forecast == res$actual)/nrow(res), "\n")
 
